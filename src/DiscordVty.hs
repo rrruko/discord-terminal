045640c5920add536fe2890df3f06d1918517aae
Ethan Jones <joneetha@oregonstate.edu>
2020-07-03 21:42:03 -0700

    enable switching current server and channel

--- src/DiscordVty.hs
+++ src/DiscordVty.hs
@@ -76,12 +76,26 @@ runClient token = mainWidget do
   let foc = fmap (bool (False, True) (True, False)) tog
   let guilds = AppState (Map.fromList
         [ (0, GuildState "guild1" (Map.fromList
-            [ (0, ChannelState "chan1" [AppMessage "buko" "chan1 message" epochTime])
-            , (1, ChannelState "chan2" [AppMessage "buko" "chan2 message" epochTime])
+            [ (0, ChannelState "chan1"
+                [ AppMessage "buko" "chan1 message" epochTime
+                ])
+            , (1, ChannelState "chan2"
+                [ AppMessage "buko" "chan2 message" epochTime
+                , AppMessage "buko" "chan2 message" epochTime
+                ])
             ]))
         , (1, GuildState "guild2" (Map.fromList
-            [ (0, ChannelState "chan3" [AppMessage "buko" "chan3 message" epochTime])
-            , (1, ChannelState "chan4" [AppMessage "buko" "chan4 message" epochTime])
+            [ (0, ChannelState "chan3"
+                [ AppMessage "buko" "chan3 message" epochTime
+                , AppMessage "buko" "chan3 message" epochTime
+                , AppMessage "buko" "chan3 message" epochTime
+                ])
+            , (1, ChannelState "chan4"
+                [ AppMessage "buko" "chan4 message" epochTime
+                , AppMessage "buko" "chan4 message" epochTime
+                , AppMessage "buko" "chan4 message" epochTime
+                , AppMessage "buko" "chan4 message" epochTime
+                ])
             ]))
         ])
   serverWidget foc (pure guilds) sendUserMessage
@@ -100,77 +114,103 @@ serverWidget
   -> (Text -> ReaderT (VtyWidgetCtx t) (Performable m) ())
   -> VtyWidget t m ()
 serverWidget foc guilds sendUserMessage = mdo
-  (currentGuildId, (userSend, currentChanId)) <-
-    splitV (pure (const 1)) (pure (True, True))
+  inp <- key V.KEsc
+  tog <- toggle False inp
+  (newGuildId, (userSend, newChanId)) <-
+    splitV (pure (const 1)) (tog <&> bool (True, False) (False, True))
       (serversView (fmap guildsMap guilds))
       (splitH
           (pure (subtract 12))
           foc
-          (boxTitle (constant def) " Server view "
+          (boxTitle (constant def) " Channel view "
             (channelView chanState))
-          (boxTitle (constant def) " Servers "
+          (boxTitle (constant def) " Channels "
             (channelsView (fmap (fmap channels) currentGuild))))
+
+  let initGuildId = guilds <&> (First . fmap fst . elemAt' 0 . guildsMap)
+  updatedGuildId <- fmap First <$> foldDyn (\x acc -> Just x) Nothing newGuildId
+  let currentGuildId = getFirst <$> mconcat
+        [ updatedGuildId, initGuildId ]
+  let initChanId = currentGuild <&> (First . fmap fst . (>>= elemAt' 0 . channels))
+  updatedChanId <- fmap First <$> foldDyn (\x acc -> Just x) Nothing newChanId
+  let currentChanId = getFirst <$> mconcat
+        [ updatedChanId, initChanId ]
   let currentGuild = (\g gId -> gId >>= \i -> (guildsMap g) Map.!? i) <$> guilds <*> currentGuildId
-  let chanState = (\s (gId, cId) -> getChannelState s gId cId)
+
+  let chanState = (\s gId cId -> getChannelState s gId cId)
         <$> (fmap guildsMap guilds)
+        <*> currentGuildId
         <*> currentChanId
   performEvent_ (fmap sendUserMessage userSend)
 
+elemAt' :: Int -> Map.Map a b -> Maybe (a, b)
+elemAt' n m =
+  let
+    rest = snd (Map.splitAt n m)
+  in
+    if length rest > 0
+      then Just (Map.elemAt 0 rest)
+      else Nothing
+
+getChannelState
+  :: Map.Map GuildId GuildState
+  -> Maybe GuildId
+  -> Maybe ChannelId
+  -> Maybe ChannelState
 getChannelState s gId cId = do
   g <- gId
   c <- cId
   currGuild <- s Map.!? g
   channels currGuild Map.!? c
 
+normal = V.defAttr
+highlighted = V.withStyle V.defAttr V.standout
+highlight isFocused =
+  RichTextConfig (current isFocused <&> bool normal highlighted)
+
 serversView
   :: forall t m. (MonadVtyApp t m, MonadNodeId m)
   => Dynamic t (Map.Map GuildId GuildState)
-  -> VtyWidget t m (Dynamic t (Maybe GuildId))
+  -> VtyWidget t m (ReflexEvent t GuildId)
 serversView guilds = do
-  e <- networkView $ guilds <&> \gs ->
-    row $ fmap (fmap getFirst . mconcat) $ forM (Map.toList gs) $ \(gId, guild) -> do
-      stretch $ do
-        f <- focus
-        display (pure (guildName' guild))
-        pure (bool (First Nothing) (First $ Just gId) <$> f)
-  d <- holdDyn (constDyn Nothing) e
-  pure (join d)
+  e <- networkView $ guilds <&> \gs -> do
+    inp <- key V.KEnter
+    runLayout
+      (constDyn Orientation_Row)
+      0
+      (1 <$ inp)
+      $ fmap (fmap getFirst . mconcat) $ forM (Map.toList gs) $ \(gId, guild) -> do
+        stretch $ do
+          f <- focus
+          richText (highlight f) (pure (guildName' guild))
+          pure (bool (First Nothing) (First $ Just gId) <$> f)
+  e' <- switchHold never (fmap (fmapMaybe id . updated) e)
+  pure e'
 
 channelsView
   :: forall t m. (MonadVtyApp t m, MonadNodeId m)
   => Dynamic t (Maybe (Map.Map ChannelId ChannelState))
-  -> VtyWidget t m (Dynamic t (Maybe GuildId, Maybe ChannelId))
+  -> VtyWidget t m (ReflexEvent t ChannelId)
 channelsView channels = do
   inp <- input
-  networkView $
+  e <- networkView $
     runLayout
       (constDyn Orientation_Column)
       0
       (1 <$ inp)
       <$> (channelsList channels)
-  pure (pure (Just 0, Just 0))
+  switchHold never $ fmap (fmapMaybe id) e
   where
-  normal = V.defAttr
-  highlighted = V.withStyle V.defAttr V.standout
-  highlight isFocused =
-    RichTextConfig (current isFocused <&> bool normal highlighted)
   channelsList chans =
     chans <&> \chs ->
       case chs of
-        Nothing -> pure [()]
-        Just c -> forM (Map.toList c) \(id, chan) -> do
+        Nothing -> pure never
+        Just c -> fmap (updated . fmap getFirst . mconcat) $ forM (Map.toList c) \(cId, chan) -> do
           fixed 1 $ do
             inp <- input
             f <- focus
             richText (highlight f) (pure (channelName' chan))
-          pure ()
-  elemAt' n m =
-    let
-      rest = snd (Map.splitAt n m)
-    in
-      if length rest > 0
-        then Just (Map.elemAt 0 rest)
-        else Nothing
+            pure (bool (First Nothing) (First $ Just cId) <$> f)
 
 sendMessageWidget
   :: (Reflex t, MonadHold t m, MonadFix m, MonadNodeId m)
